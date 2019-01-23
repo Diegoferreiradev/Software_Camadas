410b072f6705b45599864a90b19ffa298fa8a9e4
Diego Ferreira <diegoferreiradev@gmail.com>
2019-01-22 22:54:05 -0300

    Interface

---
+++ HouseDJ.Repositorios.Comum/IRepositorioGenerico.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace HouseDJ.Repositorios.Comum
+{
+    public interface IRepositorioGenerico<TEntidade, TChave>
+        where TEntidade : class
+    {
+        List<TEntidade> Selecionar();
+        TEntidade SelecionarPorId(TChave id);
+        void Inserir(TEntidade entidade);
+        void Alterar(TEntidade entidade);
+        void Excluir(TEntidade entidade);
+        void ExcluirPorId(TChave id);
+
+    }
+}
